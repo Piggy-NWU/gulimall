78f495de1b0838a9a1da6d50ac75deb8eae8648e
Bone <zxwhbjs@gmail.com>
2023-12-13 08:04:16 +0800

    完成member

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/dao/MemberReceiveAddressDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.member.dao;
+
+import com.atguigu.gulimall.member.entity.MemberReceiveAddressEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 会员收货地址
+ * 
+ * @author Bone
+ * @email zxwhbjs@gmail.com
+ * @date 2023-12-13 07:47:18
+ */
+@Mapper
+public interface MemberReceiveAddressDao extends BaseMapper<MemberReceiveAddressEntity> {
+	
+}
---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/dao/MemberStatisticsInfoDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.member.dao;
+
+import com.atguigu.gulimall.member.entity.MemberStatisticsInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 会员统计信息
+ * 
+ * @author Bone
+ * @email zxwhbjs@gmail.com
+ * @date 2023-12-13 07:47:18
+ */
+@Mapper
+public interface MemberStatisticsInfoDao extends BaseMapper<MemberStatisticsInfoEntity> {
+	
+}
