e40698d7ec0f38eec8a48e3218e17b51618ca8d5
Bone <zxwhbjs@gmail.com>
2024-01-07 20:48:59 +0800

    新增third-party模块，支持oss服务上传文件

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -21,33 +21,6 @@ public class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
 
-    @Autowired(required = false)
-    OSSClient ossClient;
-
-    @Test
-    public void test() {
-
-    }
-
-    @Test
-    public void testUpload() throws FileNotFoundException {
-  /*     // endpoint以杭州为例。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 云账号所有API访问权限
-        String accessKeyId = "LTAI5tPYUXLdjS36fqR5fMPp";
-        String accessKeySecret = "2o387fEHIpSsBjqcVz36WaI9RElEo6";
-        // 创建oss实例
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-*/
-        // 上传文件流
-        InputStream inputStream = new FileInputStream("src/test/resources/img.png");
-        ossClient.putObject("gulimall-zzz2024", "img2.png", inputStream);
-
-        // 关闭OSSClient
-        ossClient.shutdown();
-        System.out.println("上传结束");
-    }
-
     @Test
     public void contextContent() {
         BrandEntity brandEntity = new BrandEntity();
--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,14 +1,41 @@
 package com.atguigu.gulimall.thirdparty;
 
 
+import com.aliyun.oss.OSSClient;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
-class GulimallThirdPartyApplicationTests {
+@RunWith(SpringRunner.class)
+public class GulimallThirdPartyApplicationTests {
+    // 生成的对象中需要的信息都已配在nacos里。 或者参考product工程下的application.yml文件
+    @Autowired(required = false)
+    OSSClient ossClient;
 
     @Test
-    void contextLoads() {
+    public void testUpload() throws FileNotFoundException {
+  /*     // endpoint以杭州为例。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 云账号所有API访问权限
+        String accessKeyId = "LTAI5tPYUXLdjS36fqR5fMPp";
+        String accessKeySecret = "2o387fEHIpSsBjqcVz36WaI9RElEo6";
+        // 创建oss实例
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+*/
+        // 上传文件流
+        InputStream inputStream = new FileInputStream("src/test/resources/img.png");
+        ossClient.putObject("gulimall-zzz2024", "hhhhh2.png", inputStream);
+
+        // 关闭OSSClient
+        ossClient.shutdown();
+        System.out.println("上传结束");
     }
 
 }
