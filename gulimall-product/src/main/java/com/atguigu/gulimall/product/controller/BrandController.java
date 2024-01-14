4a75fb70ec4bca6eb58df5e19e2c876ca9013e9a
Bone <zxwhbjs@gmail.com>
2024-01-14 23:34:31 +0800

    p66完成.使用javax.validation做了入参的校验。比如设置某些入参不能为空，或者入参必须符合什么条件. 直接通过注解就约束好了

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -1,21 +1,20 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -24,6 +23,11 @@ import com.atguigu.common.utils.R;
  * @author Bone
  * @email zxwhbjs@gmail.com
  * @date 2023-12-10 16:35:15
+ * 3、JSR303校验
+ * 1)、给Bean类(entity类)添加校验注解 @NotBlank  @NotEmpty—— javax.validation.constraints,并定义自己的message提示
+ * 2)、在开启校验功能@Valid. PostMapping的参数前添加@Valid注解.会有默认的错误响应。
+ * 3)、给校验的bean后紧跟一个BindingResult，就可以获取到校验的结果。
+ * 4)、
  */
 @RestController
 @RequestMapping("product/brand")
@@ -36,7 +40,7 @@ public class BrandController {
      */
     @RequestMapping("/list")
 //    @RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -48,8 +52,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
 //    @RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -57,10 +61,22 @@ public class BrandController {
     /**
      * 保存
      */
-    @RequestMapping("/save")
+    @PostMapping("/save")
 //    @RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Valid @RequestBody BrandEntity brand, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> map = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(item -> {
+                // 获取错误提示
+                String message = item.getDefaultMessage();
+                // 获取错误的属性名字
+                String field = item.getField();
+                map.put(field, message);
+            });
+            return R.error(400, "提交的数据不合法").put("data", map);
+        } else {
+            brandService.save(brand);
+        }
 
         return R.ok();
     }
@@ -70,8 +86,8 @@ public class BrandController {
      */
     @RequestMapping("/update")
 //    @RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -81,8 +97,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
 //    @RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
