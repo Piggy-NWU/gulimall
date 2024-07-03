11e6705e3e2008bc3564071a43f5f98f2d9c92e8
Bone <zxwhbjs@gmail.com>
2024-07-03 21:44:25 +0800

    p94完成，上一个commit完成的是p93

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -29,6 +33,63 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.save(skuInfoEntity);
+    }
+
+
+    /*
+key: '华为',//检索关键字
+catelogId: 0,  前端是错的...后端部分是错的。 目前这张表的字段是catalogId
+brandId: 0,
+min: 0,
+max: 0
+    * */
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.and(w -> {
+                w.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        String catalogId = (String) params.get("catelogId");
+        if (StringUtils.isNotEmpty(catalogId) && !"0".equalsIgnoreCase(catalogId)) {
+            wrapper.eq("catalog_id", catalogId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if (StringUtils.isNotEmpty(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            wrapper.eq("brand_id", brandId);
+        }
+
+        String min = (String) params.get("min");
+        if (StringUtils.isNotEmpty(min)) {
+            wrapper.ge("price", min);
+        }
+
+        String max = (String) params.get("max");
+        if (StringUtils.isNotEmpty(max)) {
+            try {
+                BigDecimal maxDecimal = new BigDecimal(max);
+                // 默认最小值，最大值都是0， 如果不加一个判断，就会出现最小值大于0，最大值小于0，没有数据满足的故障了。
+                if (maxDecimal.compareTo(new BigDecimal(0)) == 1) {
+                    wrapper.le("price", max);
+                }
+            } catch (Exception e) {
+
+            }
+        }
+
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                wrapper
+        );
+
+        return new PageUtils(page);
     }
 
 }
