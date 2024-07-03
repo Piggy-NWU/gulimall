4de717086856d7509c83ada4d62a7d90e3761002
Bone <zxwhbjs@gmail.com>
2024-07-03 23:44:05 +0800

    P95结束，比较快, 可以看着接口自己写，然后再看视频

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -21,6 +24,28 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
                 new QueryWrapper<WareInfoEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+
+        if (StringUtils.isNotEmpty(key)) {
+            wrapper.and(w -> {
+                w.eq("id", key).or().like("name", key)
+                        .or().like("address", key)
+                        .or().like("areacode", key);
+            });
+        }
+
+        IPage<WareInfoEntity> page = this.page(
+                new Query<WareInfoEntity>().getPage(params),
+                wrapper
         );
 
         return new PageUtils(page);
