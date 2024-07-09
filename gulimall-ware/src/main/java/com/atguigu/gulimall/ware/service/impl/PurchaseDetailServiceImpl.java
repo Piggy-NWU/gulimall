fbfd922d017f6ea756e7e7256d83fa88d505a3f6
Bone <zxwhbjs@gmail.com>
2024-07-09 23:50:11 +0800

    P96完成，实现了查询商品库存和查询采购需求。

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,6 +26,36 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
                 new QueryWrapper<PurchaseDetailEntity>()
         );
 
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotEmpty(key)) {
+            //purchase_id  sku_id
+            wrapper.and(w -> {
+                w.eq("purchase_id", key).or().eq("sku_id", key);
+            });
+        }
+
+        String status = (String) params.get("status");
+        if (StringUtils.isNotEmpty(status)) {
+            wrapper.eq("status", status);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotEmpty(wareId)) {
+            wrapper.eq("ware_id", wareId);
+        }
+
+
+        IPage<PurchaseDetailEntity> page = this.page(
+                new Query<PurchaseDetailEntity>().getPage(params),
+                wrapper
+        );
         return new PageUtils(page);
     }
 
--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,7 +1,10 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -21,6 +24,30 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
                 new QueryWrapper<WareSkuEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+
+        if (params.containsKey("wareId")) {
+            String wareId = (String) params.get("wareId");
+            if (StringUtils.isNotEmpty(wareId)) {
+                wrapper.eq("ware_id", wareId);
+            }
+        }
+
+        String skuId = (String) params.get("skuId");
+        if (StringUtils.isNotEmpty(skuId)) {
+            wrapper.eq("sku_id", skuId);
+        }
+
+        IPage<WareSkuEntity> page = this.page(
+                new Query<WareSkuEntity>().getPage(params),
+                wrapper
         );
 
         return new PageUtils(page);
