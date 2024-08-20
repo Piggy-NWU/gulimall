6ee4cfecd987fc5fdf290939616733196cd6290a
Bone <zxwhbjs@gmail.com>
2024-08-20 23:58:30 +0800

    P126 全部完成

--- gulimall-search/src/main/java/com/atguigu/gulimallsearch/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/atguigu/gulimallsearch/config/GulimallElasticSearchConfig.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimallsearch.config;
 
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -10,6 +11,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GulimallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        // 此处省略关于超时和token的设置，可以查看官方文档.
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- gulimall-search/src/test/java/com/atguigu/gulimallsearch/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimallsearch/GulimallSearchApplicationTests.java
@@ -1,12 +1,20 @@
 package com.atguigu.gulimallsearch;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimallsearch.config.GulimallElasticSearchConfig;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @Slf4j
 @SpringBootTest
@@ -15,10 +23,30 @@ public class GulimallSearchApplicationTests {
     @Autowired
     private RestHighLevelClient client;
 
+    /*
+     * 测试存储数据到es
+     * 也可以用于更新数据到es
+     * */
     @Test
-    public void contextLoads() {
-        System.out.println(client);
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1"); // 数据的id，不填会自动生成
 
+        User user = new User();
+        user.setAge(30);
+        user.setName("zxw");
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON);
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String name;
+        private String gender;
+        private Integer age;
     }
 
 }
